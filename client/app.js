9a38d2842e96110f3340774de18b0bbda2fb49e3
Joseph Bogan <joseph.bogan@itv.com>
2021-07-09 16:52:08 +0100

    commited

--- client/app.js
+++ client/app.js
@@ -1,26 +1,214 @@
 function init() {
-  const initialiserButton = document.querySelector("#initialiser_button");
+  const joinRoomButton = document.querySelector("#join_room_button");
+  const createRoomButton = document.querySelector("#create_room_button");
+  const joinRoomInput = document.querySelector("#room_id_input");
+  const roomCode = document.querySelector("#room_code");
 
-  const createRoom = (activeTab) => {
-    chrome.scripting.executeScript({
-      target: { tabId: activeTab.id },
-      files: ["socket.io.min.js"],
-    });
+  console.log(joinRoomButton);
 
-    chrome.scripting.executeScript({
-      target: { tabId: activeTab.id },
-      files: ["overlay.js"],
-    });
-
-    chrome.scripting.insertCSS({
-      target: { tabId: activeTab.id },
-      files: ["style.css"],
-    });
+  const generateRoomId = () => {
+    const options = "abcdefghifjlmnopqrstuvwxyzABCDEFGHIFJLMNOPQRSTUVWXYZ";
+    const roomLength = 6;
+    let roomId = "";
+    for (let i = 0; i < roomLength; i++) {
+      const index = Math.floor(Math.random() * options.length);
+      roomId += options[index];
+    }
+    return roomId;
   };
 
-  const redirectToItv = () => {
-    const url = "http://itv.com/";
-    chrome.tabs.create({ url });
+  const generateName = () => {
+    const animals = [
+      "🙈",
+      "🙉",
+      "🙊",
+      "🐵",
+      "🐒",
+      "🦍",
+      "🐶",
+      "🐕",
+      "🐩",
+      "🐺",
+      "🦊",
+      "🦝",
+      "🐱",
+      "🐈",
+      "🐈",
+      "🦁",
+      "🐯",
+      "🐅",
+      "🐆",
+      "🐴",
+      "🐎",
+      "🦄",
+      "🦓",
+      "🦌",
+      "🐮",
+      "🐂",
+      "🐃",
+      "🐄",
+      "🐷",
+      "🐖",
+      "🐗",
+      "🐏",
+      "🐑",
+      "🐐",
+      "🐪",
+      "🐫",
+      "🦙",
+      "🦒",
+      "🐘",
+      "🦏",
+      "🦛",
+      "🐭",
+      "🐁",
+      "🐀",
+      "🐹",
+      "🐰",
+      "🐇",
+      "🐿",
+      "🦔",
+      "🦇",
+      "🐻",
+      "🐻",
+      "🐨",
+      "🐼",
+      "🦘",
+      "🦡",
+      "🦃",
+      "🐔",
+      "🐓",
+      "🐣",
+      "🐤",
+      "🐥",
+      "🐦",
+      "🐧",
+      "🕊",
+      "🦅",
+      "🦆",
+      "🦢",
+      "🦉",
+      "🦚",
+      "🦜",
+      "🐸",
+      "🐊",
+      "🐢",
+      "🦎",
+      "🐍",
+      "🐲",
+      "🐉",
+      "🦕",
+      "🦖",
+      "🐳",
+      "🐋",
+      "🐬",
+      "🐟",
+      "🐠",
+      "🐡",
+      "🦈",
+      "🐙",
+      "🐌",
+      "🦋",
+      "🐛",
+      "🐜",
+      "🐝",
+      "🐞",
+      "🦗",
+      "🕷",
+      "🦂",
+      "🦟",
+    ];
+    const idPart2 = Math.floor(Math.random() * 100);
+    const animalsIndex = Math.floor(Math.random() * animals.length);
+    return animals[animalsIndex] + "-" + idPart2;
+  };
+  const getName = () => {
+    if (!localStorage.getItem("userName")) {
+      const userName = generateName();
+      localStorage.setItem("userName", userName);
+    }
+    return localStorage.getItem("userName");
+  };
+
+  const createRoomInit = () => {
+    getName();
+    const roomId = generateRoomId();
+    socketFunctionality(roomId);
+    injectScript();
+  };
+
+  const joinRoomInit = () => {
+    getName();
+    socketFunctionality(joinRoomInput.value);
+  };
+
+  const socketFunctionality = (roomId) => {
+    const socket = io("ws://localhost:8080", {
+      extraHeaders: {
+        "my-custom-header": "abcd",
+      },
+    });
+
+    socket.emit("joinRoom", { roomId, userName: getName() });
+
+    socket.on("connect", () => {
+      const userName = getName();
+      socket.emit("serverUrlCheck", roomId);
+      console.log("connected to server");
+    });
+
+    socket.on("joinedRoom", (data) => {
+      console.log("joinedRoomData", data);
+      joinRoomInput.value = data.roomId;
+      roomCode.innerHTML = "Room Code: " + data.roomId;
+
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const activeTab = tabs[0];
+
+        if (activeTab.url !== data.url) {
+          chrome.tabs.update(activeTab.id, { url: data.url });
+        }
+        console.log(chrome.tabs.executeScript);
+
+        chrome.scripting.executeScript({
+          target: { tabId: activeTab.id },
+          files: ["socket.io.min.js"],
+        });
+
+        chrome.scripting.executeScript({
+          target: { tabId: activeTab.id },
+          files: ["overlay.js"],
+        });
+
+        chrome.scripting.insertCSS({
+          target: { tabId: activeTab.id },
+          files: ["style.css"],
+        });
+
+        chrome.storage.sync.set({ socket: socket }, function () {
+          console.log("socket is set to " + socket);
+        });
+
+        chrome.storage.sync.set({ roomId: roomId }, function () {
+          console.log("roomId is set to " + roomId);
+        });
+      });
+    });
+
+    socket.on("roomNotFound", () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const { url } = tabs[0];
+        console.log("Room not found url", url);
+        socket.emit("createRoom", { roomId, url });
+      });
+    });
+
+    socket.on("roomFound", (url) => {
+      if (url != Window.location) {
+        Window.location.replace(url);
+      }
+      socket.emit("joinRoom", { roomId, url });
+    });
   };
 
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -28,12 +216,82 @@ function init() {
 
     if (activeTab.url.match(/.itv.com/g)) {
       console.log("ITV URL!");
-      initialiserButton.addEventListener("click", () => createRoom(activeTab));
-    } else {
-      console.log("Not on an ITV url, redirecting to ITV.com");
-      initialiserButton.addEventListener("click", redirectToItv);
+      createRoomButton.addEventListener("click", () => createRoomInit());
+      joinRoomButton.addEventListener("click", joinRoomInit);
     }
   });
+
+  const injection = (socket, roomId) => {
+    const container = document.createElement("div");
+    const headerContainer = document.createElement("div");
+    const messagesContainer = document.createElement("div");
+    messagesContainer.classList.add("messages_container");
+    const inputContainer = document.createElement("div");
+
+    const input = document.createElement("input");
+    input.placeholder = "Start Chatting";
+    const submitButton = document.createElement("button");
+    const video = document.querySelector("video");
+
+    console.log("here", socket, roomId);
+
+    const socketFunctionality = (roomId) => {
+      socket.on("fromServer", (msg) => {
+        const text = document.createElement("div");
+        text.classList.add("message_container");
+        text.innerText = msg;
+        messagesContainer.appendChild(text);
+      });
+
+      socket.on("userJoined", (msg) => {
+        const text = document.createElement("div");
+        text.classList.add("joined_message_container");
+        text.innerText = msg + " has joined";
+        messagesContainer.appendChild(text);
+      });
+
+      socket.on("fromServerVideo", ({ action }) => {
+        console.log(action);
+        console.log(video);
+        switch (action) {
+          case "play":
+            console.log("play case");
+            if (video.paused) video.play();
+            break;
+          case "pause":
+            if (!video.paused) video.pause();
+        }
+      });
+
+      socket.on("disconnect", () => {
+        localStorage.removeItem("userName");
+      });
+
+      const sendMessage = () => {
+        const msg = getName() + ": " + input.value;
+        socket.emit("chat message", { room: roomId, msg: msg });
+      };
+
+      const sendVideoData = (action) => {
+        socket.emit("videoEvent", { room: roomId, msg: { action } });
+      };
+      submitButton.addEventListener("click", sendMessage);
+      video.addEventListener("play", () => sendVideoData("play"));
+      video.addEventListener("pause", () => sendVideoData("pause"));
+    };
+
+    container.classList.add("chat_container");
+    submitButton.innerText = "Send Message";
+    container.innerHTML = "<h1>CHAT ROOM</h1>";
+
+    container.appendChild(headerContainer);
+    container.appendChild(messagesContainer);
+    container.appendChild(inputContainer);
+
+    inputContainer.appendChild(input);
+    inputContainer.appendChild(submitButton);
+    document.body.appendChild(container);
+  };
 }
 
 document.addEventListener("DOMContentLoaded", init);
--- client/overlay.js
+++ client/overlay.js
@@ -8,52 +8,30 @@ const input = document.createElement("input");
 input.placeholder = "Start Chatting";
 const submitButton = document.createElement("button");
 const video = document.querySelector("video");
-const createRoom = document.createElement("button");
-const joinRoomInput = document.createElement("input");
-joinRoomInput.placeholder = "Enter Room Code";
-const joinRoomButton = document.createElement("button");
 
-createRoom.innerText = "Create Room";
-joinRoomButton.innerText = "Join Room";
+chrome.storage.sync.get(["socket"], function (result) {
+  const socket = result;
 
-const generateRoomId = () => {
-  const options = "abcdefghifjlmnopqrstuvwxyzABCDEFGHIFJLMNOPQRSTUVWXYZ";
-  const roomLength = 6;
-  let roomId = "";
-  for (let i = 0; i < roomLength; i++) {
-    const index = Math.floor(Math.random() * options.length);
-    roomId += options[index];
-  }
-  return roomId;
-};
+  chrome.storage.sync.get(["roomId"], function (result) {
+    const roomId = result;
 
-const createRoomInit = () => {
-  const roomId = generateRoomId();
-  socketFunctionality(roomId);
-};
-
-const joinRoomInit = () => {
-  socketFunctionality(joinRoomInput.value);
-};
-
-const socketFunctionality = (roomId) => {
-  const idHeader = document.createElement("div");
-  idHeader.innerText = `Room ID: ${roomId}`;
-  headerContainer.appendChild(idHeader);
-  const socket = io("ws://localhost:8080", {
-    extraHeaders: {
-      "my-custom-header": "abcd",
-    },
+    socketFunctionality(socket, roomId);
   });
-  socket.on("connect", () => {
-    console.log("connected to server");
-    socket.emit("joinRoom", roomId);
-  });
+});
 
+const socketFunctionality = (socket, roomId) => {
+  console.log(socket);
   socket.on("fromServer", (msg) => {
     const text = document.createElement("div");
     text.classList.add("message_container");
     text.innerText = msg;
+    messagesContainer.appendChild(text);
+  });
+
+  socket.on("userJoined", (msg) => {
+    const text = document.createElement("div");
+    text.classList.add("joined_message_container");
+    text.innerText = msg + " has joined";
     messagesContainer.appendChild(text);
   });
 
@@ -70,9 +48,13 @@ const socketFunctionality = (roomId) => {
     }
   });
 
+  socket.on("disconnect", () => {
+    localStorage.removeItem("userName");
+  });
+
   const sendMessage = () => {
-    console.log("here");
-    socket.emit("chat message", { room: roomId, msg: input.value });
+    const msg = getName() + ": " + input.value;
+    socket.emit("chat message", { room: roomId, msg: msg });
   };
 
   const sendVideoData = (action) => {
@@ -87,16 +69,10 @@ container.classList.add("chat_container");
 submitButton.innerText = "Send Message";
 container.innerHTML = "<h1>CHAT ROOM</h1>";
 
-createRoom.addEventListener("click", createRoomInit);
-joinRoomButton.addEventListener("click", joinRoomInit);
-
 container.appendChild(headerContainer);
 container.appendChild(messagesContainer);
 container.appendChild(inputContainer);
 
-headerContainer.appendChild(joinRoomInput);
-headerContainer.appendChild(createRoom);
-headerContainer.appendChild(joinRoomButton);
 inputContainer.appendChild(input);
 inputContainer.appendChild(submitButton);
 document.body.appendChild(container);
